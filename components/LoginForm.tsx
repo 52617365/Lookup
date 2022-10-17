db87b15d0be64119ad906d9f289d27cdcaba7253
52617365 <rasengithub@protonmail.com>
2022-10-17 10:46:17 +0300

    One fix then login UI is ready.
    Need to fix checkbox not getting the check icon and then login is ready.

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -33,14 +33,14 @@ export default function LoginForm() {
               <div className="flex justify-between items-center mb-6">
                 <div className="form-group form-check">
                   <input
-                    type="checkbox"
                     className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                    id="exampleCheck3"
-                    checked
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
                   />
                   <label
-                    className="form-check-label inline-block text-gray-800"
-                    htmlFor="exampleCheck2"
+                    className="form-check-label inline-block text-blue-600"
+                    htmlFor="flexCheckDefault"
                   >
                     Remember me
                   </label>
