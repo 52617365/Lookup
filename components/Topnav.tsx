7c88513de64b3a67ab770a00ac2abd6ae6f112bf
52617365 <rasengithub@protonmail.com>
2022-10-17 11:12:49 +0300

    Added log out button
    Signed-off-by: mustafabroski <youremail@yourdomain.com>

--- components/Topnav.tsx
+++ components/Topnav.tsx
@@ -1,18 +1,21 @@
 export default function Topnav() {
-  return (
-    <>
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <a className="normal-case text-xl">Lookup</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal p-0">
-            <li>
-              <a>List of data</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <a className="normal-case text-xl">Lookup</a>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal p-0-10">
+                        <li>
+                            <a>List of data</a>
+                        </li>
+                        <li>
+                            <a>Log out</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
 }
