425ed95e70c9430241ae3b6fb3b3f690d6132fa3
52617365 <rasengithub@protonmail.com>
2022-10-18 15:04:51 +0300

    css added for toggleable button. position it next.
    Signed-off-by: mustafabroski <youremail@yourdomain.com>

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,27 +1,27 @@
 export default function Dropdown({
-  dropdownText,
-  dropdownItems,
-}: {
-  dropdownText: string;
-  dropdownItems: string[];
+                                     dropdownText,
+                                     dropdownItems,
+                                 }: {
+    dropdownText: string;
+    dropdownItems: string[];
 }) {
-  return (
-    <div className="dropdown">
-      <label tabIndex={0} className="btn m-1">
-        {dropdownText}
-      </label>
-      <ul
-        tabIndex={0}
-        className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-      >
-        {dropdownItems.map((dropdownItem: string) => {
-          return (
-            <li>
-              <a>{dropdownItem}</a>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="dropdown">
+            <label tabIndex={0} className="btn m-1">
+                {dropdownText}
+            </label>
+            <ul
+                tabIndex={0}
+                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+                {dropdownItems.map((dropdownItem: string, index: number) => {
+                    return (
+                        <li key={index}>
+                            <a>{dropdownItem}</a>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
