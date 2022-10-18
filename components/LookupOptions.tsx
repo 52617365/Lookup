a7349bd8838f59ac626735185971bb23b097cb51
52617365 <rasengithub@protonmail.com>
2022-10-18 17:58:51 +0300

    Added a toggleable on/off button for strict search.
    Signed-off-by: mustafabroski <youremail@yourdomain.com>

--- components/LookupOptions.tsx
+++ components/LookupOptions.tsx
@@ -1,9 +1,17 @@
-import ToggleButton from "./Toggle";
-
 export default function LookupOptions() {
+    // TODO: remember this state in local storage.
     return (
         <div className="grid h-20 flex-grow card rounded-box place-items-center">
-            <ToggleButton leftText={"Strict"} rightText={"Wildcard"}/>
+            <div>
+                <p className={"inline-block pl-2"}>Strict</p>
+                <label className="swap float-left">
+                    <input type="checkbox"/>
+                    <div className="swap-on">ON</div>
+                    <div className="swap-off">OFF</div>
+                </label>
+                <div className={"float-left"}>
+                </div>
+            </div>
         </div>
     )
 }
