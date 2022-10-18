5c6ec9353da43118aaf312b22e1a1bc1897c7cc8
52617365 <rasengithub@protonmail.com>
2022-10-18 09:26:21 +0300

    Lookup page done visually

--- pages/lookup.tsx
+++ pages/lookup.tsx
@@ -1,14 +1,21 @@
-import {NextPage} from "next/types";
+import { NextPage } from "next/types";
 import Topnav from "../components/Topnav";
 import TextInput from "../components/TextInput";
+import Dropdown from "../components/Dropdown";
 
 const Lookup: NextPage = () => {
-    return (
-        <>
-            <Topnav/>
-            <TextInput/>
-        </>
-    )
+  const dropDownItems = ["Strict", "Wildcard"];
+  return (
+    <>
+      <Topnav />
+      <div className="w-full">
+        <div className="text-center">
+          <TextInput placeholderText="Search for anything" />
+          <Dropdown dropdownText="Mode" dropdownItems={dropDownItems} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Lookup;
