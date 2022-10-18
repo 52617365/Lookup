ba9e110f5b0d59f81770cc16bd470e1f5aef7836
52617365 <rasengithub@protonmail.com>
2022-10-18 18:23:25 +0300

    Top nav is now more reusable.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>
    Signed-off-by: mustafabroski <youremail@yourdomain.com>

--- components/Topnav.tsx
+++ components/Topnav.tsx
@@ -1,4 +1,6 @@
-export default function Topnav() {
+import Link from "next/link";
+
+export default function Topnav({options}: { options: Array<Options> }) {
     return (
         <>
             <div className="navbar bg-base-100">
@@ -7,12 +9,15 @@ export default function Topnav() {
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal p-0-10">
-                        <li>
-                            <a>List of data</a>
-                        </li>
-                        <li>
-                            <a>Log out</a>
-                        </li>
+                        {options.map((option: Options) => {
+                            return (
+                                <li>
+                                    <Link href={option.link}>
+                                        <a>{option.text}</a>
+                                    </Link>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
