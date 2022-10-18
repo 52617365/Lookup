4e93d1c247c984ef9a52ae4d8b9783f0eb2bc404
52617365 <rasengithub@protonmail.com>
2022-10-18 09:26:21 +0300

    Lookup page done visually

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,5 +1,13 @@
-export default function TextInput() {
-    return (
-        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs"/>
-    )
+export default function TextInput({
+  placeholderText,
+}: {
+  placeholderText: string;
+}) {
+  return (
+    <input
+      type="text"
+      placeholder={placeholderText}
+      className="input input-bordered w-full max-w-xs"
+    />
+  );
 }
