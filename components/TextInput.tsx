291a60d40c089851d065d09e2c139a8738e289ad
52617365 <rasengithub@protonmail.com>
2022-10-18 15:32:12 +0300

    Toggle button above search bar.
    Signed-off-by: mustafabroski <youremail@yourdomain.com>

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,13 +1,13 @@
 export default function TextInput({
-  placeholderText,
-}: {
-  placeholderText: string;
+                                      placeholderText,
+                                  }: {
+    placeholderText: string;
 }) {
-  return (
-    <input
-      type="text"
-      placeholder={placeholderText}
-      className="input input-bordered w-full max-w-xs"
-    />
-  );
+    return (
+        <input
+            type="text"
+            placeholder={placeholderText}
+            className="input input-bordered w-full max-w-xs"
+        />
+    );
 }
--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,10 +1,10 @@
 export default function ToggleButton({leftText, rightText}: { leftText: string, rightText: string }) {
     return (
-        <div className="form-control w-9">
+        <div className="form-control">
             <label className="label cursor-pointer">
-                <span className="label-text pr-2">{leftText}</span>
+                <span className={"label-text pr-2"}>{leftText}</span>
                 <input type="checkbox" className="toggle" checked/>
-                <span className="label-text pl-2">{rightText}</span>
+                <span className={"label-text pl-2"}>{rightText}</span>
             </label>
         </div>
     )
