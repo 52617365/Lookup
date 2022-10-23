7bb321fd62a80b223dd2f9abe4c7469929505b9e
52617365 <rasengithub@protonmail.com>
2022-10-23 16:16:50 +0300

    Fixed problem where the string key wasn't used cuz it expected a key.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- lib/lookupQuery.ts
+++ lib/lookupQuery.ts
@@ -6,76 +6,18 @@ import {Db} from "mongodb";
 
  */
 
-async function getResultsFromQueryTypeAll(database: Db, query: string, queryType: string, strict: boolean) {
-    if (strict) {
-        return database.collection("data").find({
-            $where: function () {
-                return (this.name == "username1")
-            }
-        })
-        // return database.collection("data").find({$text: {$search: `\"${query}\"`}}).limit(1000).toArray()
-    } else {
-        return database.collection("data").find({$text: {$search: `${query}`}}).limit(1000).toArray()
-    }
-}
 
-async function getResultsFromUsername(database: Db, query: string, strict: boolean) {
+async function getMongoDbResults(database: Db, query: string, strict: boolean, queryType: string) {
     if (strict) {
-        // @ts-ignore
-        return database.collection("data").find({username: query}, {projection: {_id: 0}}).collation({
+        return database.collection("data").find({[queryType]: query}, {projection: {_id: 0}}).collation({
             locale: "en",
             strength: 2
         }).limit(500).toArray()
     } else {
         return database.collection("data").find({
-            username: new RegExp(query, "i")
-            // @ts-ignore
-        }, {projection: {_id: 0}}).collation({
-            locale: "en",
-            strength: 2
-        }).limit(500).toArray()
+            [queryType]: new RegExp(query, "i")
+        }, {projection: {_id: 0}}).limit(500).toArray()
     }
 }
 
-async function getResultsFromPassword(database: Db, query: string, strict: boolean) {
-
-}
-
-async function getResultsFromEmail(database: Db, query: string, strict: boolean) {
-
-}
-
-async function getResultsFromIp(database: Db, query: string, strict: boolean) {
-
-}
-
-async function getResultsFromDomain(database: Db, query: string, strict: boolean) {
-
-}
-
-async function getResultsFromZipcode(database: Db, query: string, strict: boolean) {
-
-}
-
-async function getResultsFromMongo(database: Db, query: string, queryType: string, strict: boolean) {
-    switch (queryType) {
-        case "username":
-            return getResultsFromUsername(database, query, strict)
-        case "password":
-            break
-        case "emailaddress":
-            break
-        case "ipaddress":
-            break
-        case "domain":
-            break
-        case "zipcode":
-            break
-    }
-}
-
-async function getResultsFromQuery(database: Db, query: string) {
-    return database.collection('data').find({$text: {$search: query}}).toArray()
-}
-
-export default getResultsFromMongo
+export default getMongoDbResults
