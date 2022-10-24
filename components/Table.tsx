9a3520f45f1ff28b39c04de0ac53d8cd3391072f
52617365 <rasengithub@protonmail.com>
2022-10-24 13:58:33 +0300

    Lookup result tables now get resized depending on size.

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,29 +1,34 @@
 export default function Table() {
-    return (
-        <div className="overflow-x-auto">
-            <div className="indicator">
-                {/* TODO: these should be dynamic */}
-                <span className="indicator-item badge badge-primary mt-2.5">database</span>
-                <span className="indicator-item indicator-bottom badge badge-secondary mb-2.5">2019</span>
-                <table className="table w-52 border">
-                    <thead>
-                    <tr>
-                        {/* TODO: these should be dynamically rendered */}
-                        <th>Name</th>
-                        <th>Job</th>
-                        <th>Favorite Color</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {/* TODO: these should be dynamically rendered */}
-                    <tr>
-                        <td>Cy Ganderton</td>
-                        <td>Quality Control Specialist</td>
-                        <td>Blue</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
+  return (
+    // TODO: add sizing depending on resolution here onto tables, else it goes off of the screen on mobile.
+    <div className="overflow-x-auto">
+      <div className="indicator">
+        {/* TODO: these should be dynamic */}
+        <span className="indicator-item badge badge-primary mt-2.5">
+          database
+        </span>
+        <span className="indicator-item indicator-bottom badge badge-secondary mb-2.5">
+          2019
+        </span>
+        <table className="table w-52 border">
+          <thead>
+            <tr>
+              {/* TODO: these should be dynamically rendered */}
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* TODO: these should be dynamically rendered */}
+            <tr>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
--- pages/lookup.tsx
+++ pages/lookup.tsx
@@ -1,70 +1,89 @@
-import {NextPage} from "next/types";
+import { NextPage } from "next/types";
 import Topnav from "../components/Topnav";
 import TextInput from "../components/TextInput";
 import LookupOptions from "../components/LookupOptions";
 import Table from "../components/Table";
-import {useState} from "react";
-
+import { useState } from "react";
 
 // TODO: the result tables should scale accordingly depending on user resolution.
 // E.g. show n amount of rows side by side depending on user resolution.
-async function requestToEndpoint(query: string, queryType: string, strict: boolean) {
-    const rawResponse = await fetch('http://localhost:3000/api/lookup', { //TODO: change url to something real once hosted.
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({strict: strict, query: query, queryType: queryType})
-    });
-    return await rawResponse.json()
+async function requestToEndpoint(
+  query: string,
+  queryType: string,
+  strict: boolean
+) {
+  const rawResponse = await fetch("http://localhost:3000/api/lookup", {
+    //TODO: change url to something real once hosted.
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      strict: strict,
+      query: query,
+      queryType: queryType,
+    }),
+  });
+  return await rawResponse.json();
 }
 
 const Lookup: NextPage = () => {
-    const topNavLinks: Array<Options> = [{link: "/data", text: "List of data"}, {link: "/logout", text: "Log out"}]
-    const [databaseResults, setResults] = useState([])
-    const [databaseResultError, setError] = useState(false)
-    const fetchFromMongo = async (query: string, queryType: string, strict: boolean) => {
-        try {
-            const response = await requestToEndpoint(query, queryType, strict)
-            setResults(response)
-            setError(false)
-            return
-        } catch (_) {
-            setError(true)
-            return
-        }
+  const topNavLinks: Array<Options> = [
+    { link: "/data", text: "List of data" },
+    { link: "/logout", text: "Log out" },
+  ];
+  const [databaseResults, setResults] = useState([]);
+  const [databaseResultError, setError] = useState(false);
+  const fetchFromMongo = async (
+    query: string,
+    queryType: string,
+    strict: boolean
+  ) => {
+    try {
+      const response = await requestToEndpoint(query, queryType, strict);
+      setResults(response);
+      setError(false);
+      return;
+    } catch (_) {
+      setError(true);
+      return;
     }
+  };
 
-    return (
-        <>
-            <Topnav options={topNavLinks}/>
-            <LookupOptions/>
-            <div className="text-center">
-                <TextInput placeholderText="Search for anything"/>
-                <button className="btn" onClick={() => fetchFromMongo("todo", "todo", false)}>Search</button>
-                {/*TODO: set parameters here*/}
-                {/*<div className="grid grid-cols-2 gap-4 pt-5 ">*/}
-                <div className="grid row-start-2 row-end-auto pt-5 ">
-                    <div className="...">
-                        <Table/>
-                    </div>
-                    <div className="...">
-                        <Table/>
-                    </div>
-                    <div className="...">
-                        <Table/>
-                    </div>
-                    <div className="...">
-                        <Table/>
-                    </div>
-                    <div className="...">
-                        <Table/>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Topnav options={topNavLinks} />
+      <LookupOptions />
+      <div className="text-center">
+        <TextInput placeholderText="Search for anything" />
+        <button
+          className="btn"
+          onClick={() => fetchFromMongo("todo", "todo", false)}
+        >
+          Search
+        </button>
+        {/* TODO: set parameters here */}
+        <div className="grid sm:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 pt-5 ">
+          <div className="...">
+            <Table />
+          </div>
+          <div className="...">
+            <Table />
+          </div>
+          <div className="...">
+            <Table />
+          </div>
+          <div className="...">
+            <Table />
+          </div>
+          <div className="...">
+            <Table />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Lookup;
