f8a9f7c05c1f4fd15707a2ee3234f5e2df8cecbb
52617365 <rasengithub@protonmail.com>
2022-10-24 14:31:06 +0300

    Names added for lookup options.

--- components/LookUpOptionGroup.tsx
+++ components/LookUpOptionGroup.tsx
@@ -3,19 +3,19 @@ export default function LookUpOptionButtons() {
     <div className="btn-group">
       <input
         type="radio"
-        name="options"
+        name="username"
         data-title="username"
         className="btn"
       />
       <input
         type="radio"
-        name="options"
+        name="password"
         data-title="password"
         className="btn"
       />
-      <input type="radio" name="options" data-title="email" className="btn" />
-      <input type="radio" name="options" data-title="ip" className="btn" />
-      <input type="radio" name="options" data-title="domain" className="btn" />
+      <input type="radio" name="email" data-title="email" className="btn" />
+      <input type="radio" name="ipaddress" data-title="ip" className="btn" />
+      <input type="radio" name="domain" data-title="domain" className="btn" />
     </div>
   );
 }
