76aceed42920ba14b37715c2c6e5bae64679cae4
52617365 <rasengithub@protonmail.com>
2022-10-26 21:04:19 +0300

    New TODO.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- pages/lookup.tsx
+++ pages/lookup.tsx
@@ -1,10 +1,8 @@
-import { NextPage } from "next/types";
-import { useRef } from "react";
+import {NextPage} from "next/types";
+import {useRef, useState} from "react";
 import Topnav from "../components/Topnav";
 import TextInput from "../components/TextInput";
 import LookupOptions from "../components/LookupOptions";
-import Table from "../components/Table";
-import { useState } from "react";
 import LookUpOptionButtons from "../components/LookUpOptionButtons";
 
 /* 26.10.2022 - 
@@ -12,101 +10,98 @@ Have a function that renders database results, on top of this;
  handle that we have actually fetched to avoid rendering "no results found"
  on page load when user has not even queried yet.
 
- FIXME: there is currently a problem where the first search button 
-    click does not register the new query options, 
-    the changed query options get changed on the second search click.
- Fix it so that it always updates the settings on the first search button click
+Try to use next.js useSWR to do the fetching from the endpoint.
 */
 
 // TODO: function to render database results into table.
 
 // E.g. show n amount of rows side by side depending on user resolution.
 async function requestToEndpoint(
-  query: string,
-  queryType: string,
-  strict: boolean
+    query: string,
+    queryType: string,
+    strict: boolean
 ) {
-  const rawResponse = await fetch("http://localhost:3000/api/lookup", {
-    //TODO: change url to something real once hosted.
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      strict: strict,
-      query: query,
-      queryType: queryType,
-    }),
-  });
-  return await rawResponse.json();
+    const rawResponse = await fetch("http://localhost:3000/api/lookup", {
+        //TODO: change url to something real once hosted.
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            strict: strict,
+            query: query,
+            queryType: queryType,
+        }),
+    });
+    return await rawResponse.json();
 }
 
 const Lookup: NextPage = () => {
-  const topNavLinks: Array<Options> = [
-    { link: "/data", text: "List of data" },
-    { link: "/logout", text: "Log out" },
-  ];
-  const [lookupOption, setLookupOption] = useState("");
-  const [isWildcard, setWildcard] = useState(false);
+    const topNavLinks: Array<Options> = [
+        {link: "/data", text: "List of data"},
+        {link: "/logout", text: "Log out"},
+    ];
+    const [lookupOption, setLookupOption] = useState("");
+    const [isWildcard, setWildcard] = useState(false);
 
-  const [databaseResults, setResults] = useState([]);
+    const [databaseResults, setResults] = useState([]);
 
-  const fetchFromMongo = async (
-    query: string,
-    queryType: string,
-    wildcard: boolean
-  ) => {
-    try {
-      console.log(`strict: ${!wildcard}`);
-      const response = await requestToEndpoint(query, queryType, !wildcard);
-      setResults(response);
-      return;
-    } catch (_) {
-      // TODO: handle this somehow, maybe by putting having an error or something.
-      return;
-    }
-  };
+    const fetchFromMongo = async (
+        query: string,
+        queryType: string,
+        wildcard: boolean
+    ) => {
+        try {
+            console.log(`strict: ${!wildcard}`);
+            const response = await requestToEndpoint(query, queryType, !wildcard);
+            setResults(response);
+            return;
+        } catch (_) {
+            // TODO: handle this somehow, maybe by putting having an error or something.
+            return;
+        }
+    };
 
-  const captureLookUpQueryRef = useRef<HTMLInputElement>(null);
-  const searchButtonHandler = async () => {
-    const lookupQuery = captureLookUpQueryRef!.current?.value;
-    if (lookupQuery == undefined || lookupQuery == "") {
-      console.log("no query provided"); // TODO: show to user or something.
-      return;
-    }
-    if (lookupOption == "") {
-      console.log("no mode provided"); // TODO: show to user or something.
-      return;
-    }
-    // TODO: query with the settings.
-    console.log(`query: ${lookupQuery}`);
-    console.log(`option: ${lookupOption}`);
-    console.log(`wildcard: ${isWildcard}`);
-    await fetchFromMongo(lookupQuery, lookupOption, isWildcard);
-    console.log(databaseResults);
-  };
+    const captureLookUpQueryRef = useRef<HTMLInputElement>(null);
+    const searchButtonHandler = async () => {
+        const lookupQuery = captureLookUpQueryRef!.current?.value;
+        if (lookupQuery == undefined || lookupQuery == "") {
+            console.log("no query provided"); // TODO: show to user or something.
+            return;
+        }
+        if (lookupOption == "") {
+            console.log("no mode provided"); // TODO: show to user or something.
+            return;
+        }
+        // TODO: query with the settings.
+        console.log(`query: ${lookupQuery}`);
+        console.log(`option: ${lookupOption}`);
+        console.log(`wildcard: ${isWildcard}`);
+        await fetchFromMongo(lookupQuery, lookupOption, isWildcard);
+        console.log(databaseResults);
+    };
 
-  return (
-    <>
-      <Topnav options={topNavLinks} />
-      <LookupOptions isWildcard={isWildcard} setWildcard={setWildcard} />
-      <div className="text-center">
-        <div className="text-center">
-          <LookUpOptionButtons lookupOptionState={setLookupOption} />
-        </div>
-        <TextInput
-          placeholderText="Search for anything"
-          inputRef={captureLookUpQueryRef}
-        />
-        {/*  TODO: there is something wrong with the onClick handler when we submit multiple times.        */}
-        <button onClick={searchButtonHandler} className="btn">
-          Search
-        </button>
-        <div className="grid sm:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 pt-5">
-          {/* TODO: render the items here into tables here from function*/}
+    return (
+        <>
+            <Topnav options={topNavLinks}/>
+            <LookupOptions isWildcard={isWildcard} setWildcard={setWildcard}/>
+            <div className="text-center">
+                <div className="text-center">
+                    <LookUpOptionButtons lookupOptionState={setLookupOption}/>
+                </div>
+                <TextInput
+                    placeholderText="Search for anything"
+                    inputRef={captureLookUpQueryRef}
+                />
+                {/*  TODO: there is something wrong with the onClick handler when we submit multiple times.        */}
+                <button onClick={searchButtonHandler} className="btn">
+                    Search
+                </button>
+                <div className="grid sm:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 pt-5">
+                    {/* TODO: render the items here into tables here from function*/}
 
-          {/* <div className="...">
+                    {/* <div className="...">
             <Table />
           </div>
           <div className="...">
@@ -121,10 +116,10 @@ const Lookup: NextPage = () => {
           <div className="...">
             <Table />
           </div> */}
-        </div>
-      </div>
-    </>
-  );
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Lookup;
