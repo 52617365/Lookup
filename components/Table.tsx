91d1431513c38f5a9d762660f81eb6f9b2b9c38f
52617365 <rasengithub@protonmail.com>
2022-10-27 11:08:20 +0300

    db name and breachdate are shown correctly now.
    Going to add padding to the indicators next, they look odd atm.

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,5 +1,8 @@
 export default function Table({ result }: { result: any }) {
   const resultDynamicKeys = Object.keys(result);
+  const breachDate = result["breachdate"];
+  const databaseName = result["database"];
+
   return (
     // TODO: add sizing depending on resolution here onto tables, else it goes off of the screen on mobile.
     // This is definitely not a priority though.
@@ -7,20 +10,22 @@ export default function Table({ result }: { result: any }) {
       <div className="indicator">
         {/* TODO: these should be dynamic */}
         <span className="indicator-item badge badge-primary mt-2.5">
-          database
+          {databaseName}
         </span>
         <span className="indicator-item indicator-bottom badge badge-secondary mb-2.5">
-          2019
+          {breachDate}
         </span>
         <table className="table table-compact w-full border">
           <tbody>
             {resultDynamicKeys.map((dynamicKey: string) => {
-              return (
-                <tr>
-                  <td className="font-serif">{dynamicKey}:</td>
-                  <td>{result[dynamicKey]}</td>
-                </tr>
-              );
+              if (!(dynamicKey == "database" || dynamicKey == "breachdate")) {
+                return (
+                  <tr>
+                    <td className="font-serif">{dynamicKey}:</td>
+                    <td>{result[dynamicKey]}</td>
+                  </tr>
+                );
+              }
             })}
             {/* TODO: these should be dynamically rendered */}
           </tbody>
