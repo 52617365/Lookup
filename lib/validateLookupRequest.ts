2397865e6eefa0069ad3e4d320b4a66c16c1d7c7
52617365 <rasengithub@protonmail.com>
2022-10-27 10:12:45 +0300

    we now return 400 error if query is empty.

--- lib/validateLookupRequest.ts
+++ lib/validateLookupRequest.ts
@@ -1,30 +1,53 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import {WithId} from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { WithId } from "mongodb";
 
 function lookUpModeIsInvalid(mode: string): boolean {
-    const allowedModes = ["all", "username", "password", "ipaddress", "facebookid", "linkedin", "zipcode", "phonenumber"]
-    return !allowedModes.includes(mode);
+  const allowedModes = [
+    "all",
+    "username",
+    "password",
+    "ipaddress",
+    "facebookid",
+    "linkedin",
+    "zipcode",
+    "phonenumber",
+  ];
+  return !allowedModes.includes(mode);
 }
 
-function requestBodyIsInvalid(req: NextApiRequest, res: NextApiResponse<WithId<Document> | unknown>): boolean {
-    const body: RequestBody = req.body
-    return body.query == null || body.queryType == null || lookUpModeIsInvalid(body.queryType);
+function requestBodyIsInvalid(
+  req: NextApiRequest,
+  res: NextApiResponse<WithId<Document> | unknown>
+): boolean {
+  const body: RequestBody = req.body;
+  return (
+    body.query == null ||
+    body.query == "" ||
+    body.queryType == null ||
+    lookUpModeIsInvalid(body.queryType)
+  );
 }
 
-function requestTypeIsInvalid(req: NextApiRequest, res: NextApiResponse<WithId<Document> | unknown>): boolean {
-    return (req.method !== 'POST')
+function requestTypeIsInvalid(
+  req: NextApiRequest,
+  res: NextApiResponse<WithId<Document> | unknown>
+): boolean {
+  return req.method !== "POST";
 }
 
-function userRequestIsInvalid(req: NextApiRequest, res: NextApiResponse<WithId<Document> | unknown>): boolean {
-    if (requestTypeIsInvalid(req, res)) {
-        res.status(405).send({message: 'Only POST requests allowed'})
-        return true
-    }
-    if (requestBodyIsInvalid(req, res)) {
-        res.status(400).send({message: 'Request body format was invalid'})
-        return true
-    }
-    return false
+function userRequestIsInvalid(
+  req: NextApiRequest,
+  res: NextApiResponse<WithId<Document> | unknown>
+): boolean {
+  if (requestTypeIsInvalid(req, res)) {
+    res.status(405).send({ data: "Only POST requests allowed" });
+    return true;
+  }
+  if (requestBodyIsInvalid(req, res)) {
+    res.status(400).send({ data: "Request body format was invalid" });
+    return true;
+  }
+  return false;
 }
 
-export default userRequestIsInvalid
+export default userRequestIsInvalid;
