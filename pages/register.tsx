f5d2283f6ccb763298160877128bc448699cb049
52617365 <rasengithub@protonmail.com>
2022-10-28 09:34:09 +0300

    register field order changed.

--- pages/register.tsx
+++ pages/register.tsx
@@ -8,21 +8,21 @@ export default () => {
 
         <form className="mt-6">
           <div>
-            <label htmlFor="email" className="block text-sm text-gray-800">
-              Email
+            <label htmlFor="username" className="block text-sm text-gray-800">
+              Username
             </label>
             <input
-              type="email"
+              type="text"
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
           <div className="mt-4">
             <div>
-              <label htmlFor="password" className="block text-sm text-gray-800">
-                Username
+              <label htmlFor="email" className="block text-sm text-gray-800">
+                Email
               </label>
               <input
-                type="password"
+                type="email"
                 className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
               />
               <label
