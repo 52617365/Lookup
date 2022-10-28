5f7007f99e1483b8e3a0047241ac66a5b4263f23
52617365 <rasengithub@protonmail.com>
2022-10-28 09:48:30 +0300

    Added disabled attributes to register page.
    We don't yet support register page since in the V1 we will
    only do Gmail OAUTH.

--- pages/register.tsx
+++ pages/register.tsx
@@ -13,6 +13,8 @@ export default () => {
               Username
             </label>
             <input
+              placeholder="Coming soon"
+              disabled
               type="text"
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
@@ -25,6 +27,8 @@ export default () => {
               <input
                 type="email"
                 className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                placeholder="Coming soon"
+                disabled
               />
               <label
                 htmlFor="password"
@@ -33,16 +37,21 @@ export default () => {
                 Password
               </label>
               <input
+                placeholder="Coming soon"
+                disabled
                 type="password"
                 className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
-            <a href="#" className="text-xs text-gray-600 hover:underline">
+            <Link href="#" className="text-xs text-gray-600 hover:underline">
               Forget Password?
-            </a>
+            </Link>
             <div className="mt-6">
-              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                Login
+              <button
+                className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+                disabled
+              >
+                Register
               </button>
             </div>
           </div>
