dcd494df041df9d4a801e856031e83f477f75948
52617365 <rasengithub@protonmail.com>
2022-10-31 09:40:08 +0200

    Results are now rendered in a grid TODO: ..
    Fix the fact that "no results found." text is in the left side of the
    page.  It should be in the middle instead.

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,77 +1,76 @@
-import {useState} from "react";
-import {Snackbar} from "@mui/material";
+import { useState } from "react";
+import { Snackbar } from "@mui/material";
 
 function renderBreachDate(breachDate: string) {
-    if (breachDate == null) {
-        return <></>;
-    }
-    return (
-        <span className="indicator-item indicator-bottom indicator-center badge badge-secondary w-max">
+  if (breachDate == null) {
+    return <></>;
+  }
+  return (
+    <span className="indicator-item indicator-bottom indicator-center badge badge-secondary w-max">
       {breachDate}
     </span>
-    );
+  );
 }
 
 function renderDatabaseName(databaseName: string) {
-    if (databaseName == null) {
-        return <></>;
-    }
-    return (
-        <span className="indicator-item indicator-center badge badge-primary w-max">
+  if (databaseName == null) {
+    return <></>;
+  }
+  return (
+    <span className="indicator-item indicator-center badge badge-primary w-max">
       {databaseName}
     </span>
-    );
+  );
 }
 
-function sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
+export default function Table({ result }: { result: any }) {
+  const [copied, setCopied] = useState(false);
+  const resultDynamicKeys = Object.keys(result);
+  // const breachDate = result["breachdate"];
+  // const databaseName = result["database"];
 
-export default function Table({result}: { result: any }) {
-    const [copied, setCopied] = useState(false)
-    const resultDynamicKeys = Object.keys(result);
-    // const breachDate = result["breachdate"];
-    // const databaseName = result["database"];
+  async function copyText(textToCopy: string) {
+    setCopied(true);
+    await navigator.clipboard.writeText(textToCopy);
+  }
 
-    async function copyText(textToCopy: string) {
-        setCopied(true)
-        await navigator.clipboard.writeText(textToCopy)
-    }
-
-    return (
-        <div className="overflow-x-auto pt-5 pb-5">
-            {/*<div className="indicator">*/}
-            {/*{renderDatabaseName(databaseName)}*/}
-            {/*{renderBreachDate(breachDate)}*/}
-            <table className="table table-compact w-full border">
-                <tbody>
-                {resultDynamicKeys.map((dynamicKey: string) => {
-                    // if (!(dynamicKey == "database" || dynamicKey == "breachdate")) {
-                    // we don't want to render these because they're already in the indicators.
-                    return (
-                        <>
-                            <div>
-                                <Snackbar
-                                    message="Copied to clipboard"
-                                    anchorOrigin={{vertical: "top", horizontal: "center"}}
-                                    autoHideDuration={500}
-                                    onClose={() => setCopied(false)}
-                                    open={copied}
-                                />
-                            </div>
-                            <div>
-                                <tr>
-                                    <td className="font-serif">{dynamicKey}:</td>
-                                    <td onClick={() => copyText(result[dynamicKey])}>{result[dynamicKey]}</td>
-                                </tr>
-                            </div>
-                        </>
-                    );
-                    // }
-                })}
-                </tbody>
-            </table>
-        </div>
-        // </div>
-    );
+  return (
+    // <div className="overflow-x-auto pt-5 pb-5">
+    <div className="pt-5 pb-5 grid gap-4 align-center justify-center">
+      {/*// <div className="indicator">*/}
+      {/*// {renderDatabaseName(databaseName)}*/}
+      {/*// {renderBreachDate(breachDate)}*/}
+      <table className="table table-compact border">
+        <tbody>
+          {resultDynamicKeys.map((dynamicKey: string) => {
+            // if (!(dynamicKey == "database" || dynamicKey == "breachdate")) {
+            // we don't want to render these because they're already in the indicators.
+            return (
+              <>
+                <div>
+                  <Snackbar
+                    message="Copied to clipboard"
+                    anchorOrigin={{ vertical: "top", horizontal: "center" }}
+                    autoHideDuration={500}
+                    onClose={() => setCopied(false)}
+                    open={copied}
+                  />
+                </div>
+                <div>
+                  <tr>
+                    <td className="font-serif">{dynamicKey}:</td>
+                    <td onClick={() => copyText(result[dynamicKey])}>
+                      {result[dynamicKey]}
+                    </td>
+                  </tr>
+                </div>
+              </>
+            );
+            // }
+          })}
+        </tbody>
+      </table>
+    </div>
+    // </div>
+  );
 }
--- pages/lookup.tsx
+++ pages/lookup.tsx
@@ -1,5 +1,5 @@
-import {NextPage} from "next/types";
-import {useRef, useState} from "react";
+import { NextPage } from "next/types";
+import { useRef, useState } from "react";
 import Topnav from "../components/Topnav";
 import TextInput from "../components/TextInput";
 import LookupOptions from "../components/LookupOptions";
@@ -9,129 +9,153 @@ import requestToEndpoint from "../lib/queryEndpoint";
 import Table from "../components/Table";
 import SearchButton from "../components/SearchButton";
 
-function renderQueryInformation(results: LookupApiResponse | undefined, isFetched: boolean, isError: boolean, timeTook: number) {
-    if (results == undefined) {
-        // I.e. if fetch has not completed yet.
-        return;
-    }
-    // if (isError) {
-    //     return <p>There was an error fetching the databases</p>;
-    // }
-    if (results.data.length === 0 && !isFetched) {
-        return <></>;
-    }
-    return (
-        <div className="flex flex-wrap align-items-center justify-center gap-2">
-            <p>Results: {results.data.length}</p>
-            <p>Query time: {timeTook.toFixed(0)}ms</p>
-        </div>
-    )
+function renderQueryInformation(
+  results: LookupApiResponse | undefined,
+  isFetched: boolean,
+  isError: boolean,
+  timeTook: number
+) {
+  if (results == undefined) {
+    // I.e. if fetch has not completed yet.
+    return;
+  }
+  // if (isError) {
+  //     return <p>There was an error fetching the databases</p>;
+  // }
+  if (results.data.length === 0 && !isFetched) {
+    return <></>;
+  }
+  return (
+    <div className="flex flex-wrap align-items-center justify-center gap-2 pt-2">
+      <p>Results: {results.data.length}</p>
+      <p>Query time: {timeTook.toFixed(0)}ms</p>
+    </div>
+  );
 }
 
-
 function renderDatabaseResults(
-    results: LookupApiResponse | undefined,
-    isFetched: boolean,
-    isError: boolean
+  results: LookupApiResponse | undefined,
+  isFetched: boolean,
+  isError: boolean
 ) {
-    if (results == undefined) {
-        // I.e. if fetch has not completed yet.
-        return;
-    }
-    if (isError) {
-        return <p>There was an error fetching the databases</p>;
-    }
-    if (results.data.length === 0 && !isFetched) {
-        return <></>;
-    }
-    if (isFetched && results.data.length === 0) {
-        return <p>No results found.</p>;
-    }
+  if (results == undefined) {
+    // I.e. if fetch has not completed yet.
+    return;
+  }
+  if (isError) {
+    return <p>There was an error fetching the databases</p>;
+  }
+  if (results.data.length === 0 && !isFetched) {
+    return <></>;
+  }
+  if (isFetched && results.data.length === 0) {
+    return <p>No results found.</p>;
+  }
 
-    return (
-        results.data.map((result: any) => {
-            return <Table result={result}/>;
-        })
-    )
+  return results.data.map((result: any) => {
+    return <Table result={result} />;
+  });
 }
 
 const Lookup: NextPage = () => {
-    // if (!isUserLoggedIn()) {
-    //   return (
-    //     <div className="flex items-center justify-center h-screen flex-wrap">
-    //       <Link href="/">You are unauthenticated, please login.</Link>
-    //     </div>
-    //   );
-    // }
+  // if (!isUserLoggedIn()) {
+  //   return (
+  //     <div className="flex items-center justify-center h-screen flex-wrap">
+  //       <Link href="/">You are unauthenticated, please login.</Link>
+  //     </div>
+  //   );
+  // }
 
-    const topNavLinks: Array<Options> = [
-        {link: "/data", text: "List of data"},
-        {link: "/logout", text: "Log out"}, // log out should maybe have a click handler.
-    ];
-    const [lookupOption, setLookupOption] = useState<string>("");
-    const [isWildcard, setWildcard] = useState<boolean>(false);
+  const topNavLinks: Array<Options> = [
+    { link: "/data", text: "List of data" },
+    { link: "/logout", text: "Log out" }, // log out should maybe have a click handler.
+  ];
+  const [lookupOption, setLookupOption] = useState<string>("");
+  const [isWildcard, setWildcard] = useState<boolean>(false);
 
-    const [databaseResults, setResults] = useState<LookupApiResponse>();
-    const [isDatabaseError, setDatabaseError] = useState<boolean>(false);
-    const [isFetched, setFetched] = useState<boolean>(false);
-    const [buttonIsLoading, setButtonLoading] = useState<boolean>(false);
-    const [timeTook, setTimeTook] = useState<number>(0);
+  const [databaseResults, setResults] = useState<LookupApiResponse>();
+  const [isDatabaseError, setDatabaseError] = useState<boolean>(false);
+  const [isFetched, setFetched] = useState<boolean>(false);
+  const [buttonIsLoading, setButtonLoading] = useState<boolean>(false);
+  const [timeTook, setTimeTook] = useState<number>(0);
 
-    const captureLookUpQueryRef = useRef<HTMLInputElement>(null);
-    const searchButtonHandler = async () => {
-        if (buttonIsLoading) {
-            // to avoid fetching while we fetch.
-            return;
+  const captureLookUpQueryRef = useRef<HTMLInputElement>(null);
+  const searchButtonHandler = async () => {
+    if (buttonIsLoading) {
+      // to avoid fetching while we fetch.
+      return;
+    }
+
+    const lookupQuery = captureLookUpQueryRef!.current?.value;
+    if (!isValidQuery(lookupQuery, lookupOption)) {
+      return;
+    }
+
+    setButtonLoading(true);
+    setFetched(true);
+    try {
+      const startTime = performance.now();
+      const response = await requestToEndpoint(
+        lookupQuery as string,
+        lookupOption,
+        !isWildcard
+      );
+      const endTime = performance.now();
+      setTimeTook(endTime - startTime);
+      setResults(response);
+      setDatabaseError(false);
+    } catch (_) {
+      setDatabaseError(true);
+    } finally {
+      setButtonLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Topnav options={topNavLinks} />
+      <LookupOptions isWildcard={isWildcard} setWildcard={setWildcard} />
+      <div className="text-center">
+        <div>
+          <LookUpOptionButtons setLookupOptionState={setLookupOption} />
+        </div>
+        <TextInput
+          placeholderText="Search for anything"
+          inputRef={captureLookUpQueryRef}
+        />
+        <SearchButton
+          searchButtonHandler={searchButtonHandler}
+          isLoading={buttonIsLoading}
+        />
+        {renderQueryInformation(
+          databaseResults,
+          isFetched,
+          isDatabaseError,
+          timeTook
+        )}
+      </div>
+      <div
+        className={
+          "grid justify-center md:grid-cols-3 sm:grid-cols-2 m-auto pt-5 w-5/6 gap-4"
         }
-
-        const lookupQuery = captureLookUpQueryRef!.current?.value;
-        if (!isValidQuery(lookupQuery, lookupOption)) {
-            return;
-        }
-
-        setButtonLoading(true);
-        setFetched(true);
-        try {
-            const startTime = performance.now()
-            const response = await requestToEndpoint(
-                lookupQuery as string,
-                lookupOption,
-                !isWildcard
-            );
-            const endTime = performance.now()
-            setTimeTook(endTime - startTime)
-            setResults(response);
-            setDatabaseError(false);
-        } catch (_) {
-            setDatabaseError(true);
-        } finally {
-            setButtonLoading(false);
-        }
-    };
-
-    return (
-        <>
-            <Topnav options={topNavLinks}/>
-            <LookupOptions isWildcard={isWildcard} setWildcard={setWildcard}/>
-            <div className="text-center">
-                <div>
-                    <LookUpOptionButtons setLookupOptionState={setLookupOption}/>
-                </div>
-                <TextInput
-                    placeholderText="Search for anything"
-                    inputRef={captureLookUpQueryRef}
-                />
-                <SearchButton
-                    searchButtonHandler={searchButtonHandler}
-                    isLoading={buttonIsLoading}
-                />
-                {renderQueryInformation(databaseResults, isFetched, isDatabaseError, timeTook)}
-                <div className="flex flex-wrap align-center justify-center gap-4">
-                    {renderDatabaseResults(databaseResults, isFetched, isDatabaseError)}
-                </div>
-            </div>
-        </>
-    );
+      >
+        {/* <div className="flex flex-wrap align-center flex-row gap-4"> */}
+        {/* <div className="grid gap-4 align-center justify-center">
+          lkjasdflkjasdfl;jkasdfjkl;asdjklasfd
+        </div>
+        <div className="grid gap-4 align-center justify-center">
+          lkjasdflkjasdfl;jkasdfjkl;asdjklasfd
+        </div>
+        <div className="grid gap-4 align-center justify-center">
+          lkjasdflkjasdfl;jkasdfjkl;asdjklasfd
+        </div> */}
+        {/* </div> */}
+        {/* <div className="flex flex-row flex-wrap align-center justify-center gap-4"> */}
+        {renderDatabaseResults(databaseResults, isFetched, isDatabaseError)}
+      </div>
+      {/*</div>*/}
+    </>
+  );
 };
 
 export default Lookup;
