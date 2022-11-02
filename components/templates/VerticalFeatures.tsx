bcd62ac5e96ec0861047eb1f3069eaae53f74733
52617365 <rasengithub@protonmail.com>
2022-11-02 16:03:41 +0200

    added a text field to features.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- components/templates/VerticalFeatures.tsx
+++ components/templates/VerticalFeatures.tsx
@@ -1,33 +1,33 @@
-import { VerticalFeatureRow } from "../feature/VerticalFeatureRow";
-import { Section } from "../layout/Section";
+import {VerticalFeatureRow} from "../feature/VerticalFeatureRow";
+import {Section} from "../layout/Section";
 
 const VerticalFeatures = () => (
-  <div className={"bg-gray-100"}>
-    <Section
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada nisi tellus, non imperdiet nisi tempor at."
-    >
-      <VerticalFeatureRow
-        title="Your title here"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-        image="/assets/images/feature.svg"
-        imageAlt="First feature alt text"
-      />
-      <VerticalFeatureRow
-        title="Your title here"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-        image="/assets/images/feature2.svg"
-        imageAlt="Second feature alt text"
-        reverse
-      />
-      <VerticalFeatureRow
-        title="Your title here"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-        image="/assets/images/feature3.svg"
-        imageAlt="Third feature alt text"
-      />
-    </Section>
-  </div>
+    <div className={"bg-gray-100"}>
+        <Section
+            title="Every year, over 20 billion credentials get breached"
+            description="2.5 credentials per person, your personal details might be breached, act now."
+        >
+            <VerticalFeatureRow
+                title="Your title here"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+                image="/assets/images/feature.svg"
+                imageAlt="First feature alt text"
+            />
+            <VerticalFeatureRow
+                title="Your title here"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+                image="/assets/images/feature2.svg"
+                imageAlt="Second feature alt text"
+                reverse
+            />
+            <VerticalFeatureRow
+                title="Your title here"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+                image="/assets/images/feature3.svg"
+                imageAlt="Third feature alt text"
+            />
+        </Section>
+    </div>
 );
 
-export { VerticalFeatures };
+export {VerticalFeatures};
