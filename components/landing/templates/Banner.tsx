98e825582f7a4aa948972633fd3c747b4d131ee8
52617365 <rasengithub@protonmail.com>
2022-11-02 09:09:57 +0200

    changed background of banner.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- components/landing/templates/Banner.tsx
+++ components/landing/templates/Banner.tsx
@@ -5,17 +5,19 @@ import {CTABanner} from '../cta/CTABanner';
 import {Section} from '../layout/Section';
 
 const Banner = () => (
-    <Section>
-        <CTABanner
-            title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-            subtitle="Start your Free Trial."
-            button={
-                <Link href="https://creativedesignsguru.com/category/nextjs/">
-                    <Button>Get Started</Button>
-                </Link>
-            }
-        />
-    </Section>
+    <div className={"bg-gray-100"}>
+        <Section>
+            <CTABanner
+                title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+                subtitle="Start your Free Trial."
+                button={
+                    <Link href="https://creativedesignsguru.com/category/nextjs/">
+                        <Button>Get Started</Button>
+                    </Link>
+                }
+            />
+        </Section>
+    </div>
 );
 
 export {Banner};
