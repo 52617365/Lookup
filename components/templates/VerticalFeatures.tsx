f94a7335772617b849b33ef2d758efeadc61adc1
52617365 <rasengithub@protonmail.com>
2022-11-02 16:18:12 +0200

    todo: texts for landing.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- components/templates/VerticalFeatures.tsx
+++ components/templates/VerticalFeatures.tsx
@@ -4,8 +4,8 @@ import {Section} from "../layout/Section";
 const VerticalFeatures = () => (
     <div className={"bg-gray-100"}>
         <Section
-            title="Every year, over 20 billion credentials get breached"
-            description="2.5 credentials per person, your personal details might be breached, act now."
+            title="Your title here"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.."
         >
             <VerticalFeatureRow
                 title="Your title here"
